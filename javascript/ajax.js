7348a3c830cf447536c6b33cad22f68c808839cd
Malcolm Strong <strong.malcolm@yahoo.com>
2017-06-05 22:23:23 -0400

    Edited some javascript and CSS

---
+++ javascript/ajax.js
@@ -0,0 +1,66 @@
+
+$( document ).ready(function() {
+
+var hiphop = ["Outkast", "Anderson Paak", "Kendrick Lamar", "J Cole", "Schoolboy Q", "Ludacris", "T.I.", "Drake", "Lil Wayne"]
+
+
+function displayArtistButtons(){
+    $("#buttonsView").empty();
+        var gifButton = $("<button>");
+        gifButton.addClass("hiphops");
+        gifButton.addClass("btn btn-primary")
+        gifButton.attr("data-name", hiphop[i]);
+        gifButton.text(hiphop[i]);
+        $("#buttonsView").append(gifButton);
+    }
+}
+
+function addNewArtist(){
+    $("#add-artist").on("click", function(){
+    var hiphops = $("#artists-input").val().trim();
+    if (hiphops == ""){
+      return false;
+    }
+    hiphop.push(action);
+
+    displayArtistButtons();
+    return false;
+    });
+}
+
+
+// function removeLastArtist(){
+//     $("removeGif").on("click", function(){
+//     hiphop.pop(hiphops);
+//     displayGifButtons();
+//     return false;
+//     });
+}
+
+function displayArtist(){
+    var hiphop = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + hiphop + "&api_key=dc6zaTOxFJmzC&limit=10";
+    console.log(queryURL); 
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+    .done(function(response) {
+        console.log(response);
+
+
+
+
+
+
+    // $(".gif").on("click", function() {
+    //
+    //   var state = $(this).attr("data-state");
+    //   if (state === "still") {
+    //     $(this).attr("src", $(this).attr("data-animate"));
+    //     $(this).attr("data-state", "animate");
+    //   } else {
+    //     $(this).attr("src", $(this).attr("data-still"));
+    //     $(this).attr("data-state", "still");
+    //   }
+    // });
